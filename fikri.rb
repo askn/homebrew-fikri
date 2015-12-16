83e2ac902d683adc3dfe5fdb9bda61f9ec8fd6f9
Aşkın Gedik <askn@bil.omu.edu.tr>
2015-12-16 16:11:40 +0200

    .

--- fikri.rb
+++ fikri.rb
@@ -2,7 +2,7 @@ class Fikri < Formula
   homepage "https://github.com/askn/fikri"
   url "https://github.com/askn/homebrew-fikri/archive/v0.0.2.zip"
   version "0.0.2"
-  sha256 "2fe20e37232a306303d0e972f1ae9f5d8ebac10ad6c5125304959125e3efddce"
+  sha256 "bdd206574a9e877d10f425cb100f0a5d964f610c3a99055efae1570f15fa9fe3"
 
   def install
     bin.install "fikri"
